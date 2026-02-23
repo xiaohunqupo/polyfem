3671145d470c19bc62a1a966d78772ddf037daf4
Ian Su <TzuHsiangSu3000@gmail.com>
2026-02-23 18:20:20 -0500

    remove format changes

--- src/polyfem/solver/forms/TangentialAdhesionForm.cpp
+++ src/polyfem/solver/forms/TangentialAdhesionForm.cpp
@@ -60,14 +60,11 @@ namespace polyfem::solver
 	{
 		POLYFEM_SCOPED_TIMER("tangential adhesion hessian");
 
-		ipc::PSDProjectionMethod psd_projection_method;
+		ipc::PSDProjectionMethod psd_projection_method; 
 
-		if (project_to_psd_)
-		{
+		if (project_to_psd_) {
 			psd_projection_method = ipc::PSDProjectionMethod::CLAMP;
-		}
-		else
-		{
+		} else {
 			psd_projection_method = ipc::PSDProjectionMethod::NONE;
 		}
 
