4f3580a076ebe9cbb20afbed0d45f5f7b48aa316
xDarkLemon <liuyibo97@outlook.com>
2026-03-16 00:05:01 -0700

    Update paravieo package

--- cmake/recipes/paraviewo.cmake
+++ cmake/recipes/paraviewo.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'paraviewo::paraviewo'")
 
 include(CPM)
-CPMAddPackage("gh:polyfem/paraviewo#7ca1daa874bd9f86a90fdaf5605a739b3eee76c8")
+CPMAddPackage("gh:polyfem/paraviewo#9ac491eb3a2a28b6725bf252ac3bf47c78882fb8")
