2146739526aad14899c916ea47f987087e7cead9
teseoch <teseo.schneider@gmail.com>
2026-07-16 14:24:11 -0700

    NS uses polysolve

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG d6e8be8e5723743111f7c16ef0f1fcb22724d614
+        GIT_TAG 8dab45af3ab021319b23a1d1ebebf65f4eea97b2
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
